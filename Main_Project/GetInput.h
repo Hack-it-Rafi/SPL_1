5d0f404e906820f6e09fb1e7f89593bae2794bbd
Hack-it-Rafi <113276024+Hack-it-Rafi@users.noreply.github.com>
2023-05-18 12:26:35 +0600

    Add files via upload

---
+++ Main_Project/GetInput.h
@@ -0,0 +1,16 @@
+#ifndef GETINPUT_H_INCLUDED
+#define GETINPUT_H_INCLUDED
+#include <bits/stdc++.h>
+#define RowNum 23
+#define ColNum 75
+using namespace std;
+
+int getInput()
+{
+    int x;
+    x = getch();
+    return x;
+}
+
+
+#endif // GETINPUT_H_INCLUDED
